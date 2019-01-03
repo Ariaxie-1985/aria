399cf83a86c15abcb855fa54748f162a9c31bdc2
cloudyyuan <cloudyyuan@lagou.com>
2019-01-03 15:11:48 +0800

    批量分配和刷新

--- api_script/business/Batch_Allocation.py
+++ api_script/business/Batch_Allocation.py
@@ -1,15 +1,11 @@
 #批量分配
-<<<<<<< HEAD
-from util.util import login ,get
-||||||| merged common ancestors
-from api_script.util import login ,get
-=======
+
 import json
-from api_script.BeautifulSoup import exist_class_name
-from api_script.util import login,get,form_post,get_code_token,gethtml
+from util.BeautifulSoup import exist_class_name
+from util.util import login,get,form_post,get_code_token,gethtml,assert_equal
 import logging
 logging.getLogger().setLevel(logging.INFO)
->>>>>>> 0ada134382f2f998f1ddd44079db99ca427602f4
+
 
 username = 20181205
 login("00852", username)
@@ -17,17 +13,70 @@ login("00852", username)
 def batch_allocation():
     headerurl="https://easy.lagou.com/subAccount/queryAcount/index.htm"
     header = get_code_token(headerurl)
-    jsonobject = get("https://easy.lagou.com/subAccount/queryAcount.json?pageNo=1&pageSize=7&keyword=","获取当前有几个子账号")
-    childaccount=jsonobject.get("content").get("data").get("subAcccountPage").get("totalCount")
+    jsonobject = get(url="https://easy.lagou.com/subAccount/queryAcount.json?pageNo=1&pageSize=7&keyword=",remark="获取当前有几个子账号")
+    childaccount = jsonobject.json()['content']['data']['subAcccountPage']['totalCount']
+    # childaccount=jsonobject.get("content").get("data").get("subAcccountPage").get("totalCount")
     print('当前有' + str(childaccount) + '个子账号')
     if childaccount < 2:
         #b=exist_class_name("https://easy.lagou.com/subAccount/queryAcount/index.htm")
         b=gethtml("https://easy.lagou.com/subAccount/queryAcount/index.htm")
         a=exist_class_name(b,"batch-handle-btn ")
+        assert_equal(True,a,"没有出现批量分配","出现了批量分配")
         while childaccount < 2:
                 add1=form_post(url="https://easy.lagou.com/subAccount/addAcount.json",data={'userId':100014642},remark="添加子账号",headers=header)
                 add1=form_post(url="https://easy.lagou.com/subAccount/addAcount.json",data={'userId':100014643},remark="添加子账号",headers=header)
-                jsonobject = get("https://easy.lagou.com/subAccount/queryAcount.json?pageNo=1&pageSize=7&keyword=","获取当前有几个子账号")
-                childaccount=jsonobject.get("content").get("data").get("subAcccountPage").get("totalCount")
+                jsonobject = get(url="https://easy.lagou.com/subAccount/queryAcount.json?pageNo=1&pageSize=7&keyword=",remark="获取当前有几个子账号")
+                childaccount = jsonobject.json()['content']['data']['subAcccountPage']['totalCount']
                 print('添加后' + str(childaccount) + '个子账号')
+                b=gethtml("https://easy.lagou.com/subAccount/queryAcount/index.htm")
+                a=exist_class_name(b,"batch-handle-btn ")
+                assert_equal(False,a,"出现了批量分配,正确","没有出现了批量分配,失败")
+    else:
+        b=gethtml("https://easy.lagou.com/subAccount/queryAcount/index.htm")
+        a=exist_class_name(b,"batch-handle-btn ")
+        assert_equal(False,a,"没有出现批量分配","出现了批量分配")
+
+
 batch_allocation()
+
+'''
+1、批量分配
+2、验证是否分配成功
+'''
+def batchAllocate():
+    headerurl="https://easy.lagou.com/subAccount/queryAcount/index.htm"
+    header = get_code_token(headerurl)
+    batchAllocate_url="https://easy.lagou.com/userGoodsRecord/batchAllocate"
+    batchAllocate_Data={"allocateInfo":[{"userId":"100014643","allocateDetail":[{"baseGoodsId":"704","allocateNum":"1"},
+                                                                                {"baseGoodsId":"701","allocateNum":"1"},
+                                                                                {"baseGoodsId":"706","allocateNum":"1"},
+                                                                                {"baseGoodsId":"705","allocateNum":"1"},
+                                                                                {"baseGoodsId":"614","allocateNum":"1"}]},
+                                        {"userId":"100014642","allocateDetail":[{"baseGoodsId":"704","allocateNum":"1"},
+                                                                                {"baseGoodsId":"701","allocateNum":"1"},
+                                                                                {"baseGoodsId":"706","allocateNum":"1"},
+                                                                                {"baseGoodsId":"705","allocateNum":"1"},
+                                                                                {"baseGoodsId":"614","allocateNum":"1"}]}]}
+    batchAllocate_Data["allocateInfo"] = json.dumps(batchAllocate_Data["allocateInfo"])
+    jsonobject =form_post(url=batchAllocate_url,remark="批量分配每个用户1个",data=batchAllocate_Data,headers=header)
+    actualvalue=jsonobject.get("message")
+    assert_equal("批量分配成功",actualvalue,"批量分配成功","批量分配失败")
+    headerurl="https://easy.lagou.com/subAccount/queryAcount/index.htm"
+    header = get_code_token(headerurl)
+    batchAllocate_url="https://easy.lagou.com/userGoodsRecord/batchAllocate"
+    batchAllocate_Data={"allocateInfo":[{"userId":"100014643","allocateDetail":[{"baseGoodsId":"704","allocateNum":"99999"},
+                                                                                {"baseGoodsId":"701","allocateNum":"1"},
+                                                                                {"baseGoodsId":"706","allocateNum":"1"},
+                                                                                {"baseGoodsId":"705","allocateNum":"1"},
+                                                                                {"baseGoodsId":"614","allocateNum":"1"}]},
+                                        {"userId":"100014642","allocateDetail":[{"baseGoodsId":"704","allocateNum":"1"},
+                                                                                {"baseGoodsId":"701","allocateNum":"1"},
+                                                                                {"baseGoodsId":"706","allocateNum":"1"},
+                                                                                {"baseGoodsId":"705","allocateNum":"1"},
+                                                                                {"baseGoodsId":"614","allocateNum":"1"}]}]}
+    batchAllocate_Data["allocateInfo"] = json.dumps(batchAllocate_Data["allocateInfo"])
+    jsonobject =form_post(url=batchAllocate_url,remark="批量分配每个用户1个",data=batchAllocate_Data,headers=header)
+    actualvalue=jsonobject.get("message")
+    assert_equal("批量分配异常批量分配失败，子账号需要分配的704权益总数大于父账号的剩余数量",actualvalue,"大于可分配数量，后台不允许分配","批量分配失败")
+
+batchAllocate()
--- api_script/business/Refreshed.py
+++ api_script/business/Refreshed.py
@@ -1,13 +1,34 @@
 #2018秋版合同刷新职位
-from util.util import login ,get_code_token
+#需要自己配置等待多长时间
+from util.util import login ,get_code_token,form_post,gethtml,assert_equal
+from util.BeautifulSoup import find_classname
+import time
 
 login('00852','20181205')
-def Refreshed():
+def Refreshed(second):
     '''
     18版合同“刷新职位”
     :return:
     '''
-    recruiter_reflash="https://easy.lagou.com/position/multiChannel/myOnlinePositions.htm?pageNo=1"
-    queryAcount_header = get_code_token(recruiter_reflash)
-    r = form_post(queryAcount_url, queryAcount_data, queryAcount_header)
-    return r['content']['data']['data'][0]['userid']
+    position_url = 'https://easy.lagou.com/parentPosition/multiChannel/myOnlinePositions.json'
+    position_header = get_code_token('https://easy.lagou.com/position/multiChannel/myOnlinePositions.htm')
+    s = form_post(url=position_url,headers=position_header,data={'pageNo':1},remark='获取职位id')
+    positionId=s['content']['data']['parentPositionVOs'][0]['positions'][0]['positionId']
+    print(positionId)
+    refresh_url = "https://easy.lagou.com/position/refreshPosition.json"
+    refresh_header = get_code_token("https://easy.lagou.com/position/my_online_positions.htm?pageNo=1")
+    refresh_data = {'positionId': positionId}
+    print (refresh_header)
+    jsonobject = form_post(url=refresh_url,headers=refresh_header,data=refresh_data,remark='刷新职位')
+    a=jsonobject.get("message")
+    print(a)
+
+    if a=="操作成功":
+        assert_equal("操作成功",a,"首次刷新成功","首次刷新失败")
+    else:
+        time.sleep(second)
+        jsonobject = form_post(url=refresh_url,headers=refresh_header,data=refresh_data,remark='刷新职位')
+        assert_equal("操作成功",a,"首次刷新成功","首次刷新失败")
+Refreshed(3000)
+
+
