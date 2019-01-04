b945fe21ac05bbc7c60a4128c4dbb8d93d2b6680
yqzhang <yqzhang@lagou.com>
2019-01-04 17:54:55 +0800

    解决冲突

--- util/read_yaml.py
+++ util/read_yaml.py
@@ -7,10 +7,15 @@ import os
 
 
 def get_yaml_test_data(yamlfile):
-	# 获取当前脚本所在文件夹路径
-	curPath = os.getcwd()+"\\testdata"
-	# 获取yaml文件路径
-	yamlPath = os.path.join(curPath, yamlfile)
-	# open方法打开直接读出来
-	cfg = open(yamlPath, 'r', encoding='utf-8').read()
-	return yaml.load(cfg)  # 用load方法转字典
+    # 获取当前脚本所在文件夹路径
+    if os.name == "nt":
+        curPath = os.getcwd() +"\\tests\\testdata"
+    else:
+        curPath = os.getcwd() + "/tests/testdata"
+
+    # 获取yaml文件路径
+    yamlPath = os.path.join(curPath, yamlfile)
+    # open方法打开直接读出来
+    cfg = open(yamlPath, 'r', encoding='utf-8').read()
+    return yaml.load(cfg)  # 用load方法转字典
+
