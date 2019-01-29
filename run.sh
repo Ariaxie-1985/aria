53d3d884a2b498c852cc257f43d5f51496369c85
Xiawang <xiawang@lagou.com>
2019-01-29 15:46:33 +0800

    修改配置3

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/lib/jenkins/workspace/python_api
-/usr/local/python3/bin/pipenv run pytest tests/test_B_add_people_into_company.py
+/usr/local/python3/bin/pipenv run pytest tests/test_B_add_people_into_company.py --html=report.html --self-contained-html -p no:warnings
 python3 utils/send_email.py
