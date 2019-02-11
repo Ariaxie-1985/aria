be7ce3da11ca51cef9c1eac23aee433b818ffbdb
Xiawang <xiawang@lagou.com>
2019-02-11 19:29:22 +0800

    在run_business.sh移除非商业测试脚本

--- run_business.sh
+++ run_business.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/local/python3/bin/pipenv run pytest tests/test_Akeytorefresh.py tests/test_B_calling.py tests/test_B_energycard.py tests/test_B_refresh.py tests/test_LagouPlus.py tests/test_Refreshed.py tests/test_SwitchingContract.py tests/test_app_b_position.py tests/test_sub_account.py
+/usr/local/python3/bin/pipenv run pytest tests/test_Akeytorefresh.py tests/test_B_calling.py tests/test_B_energycard.py tests/test_B_refresh.py tests/test_LagouPlus.py tests/test_Refreshed.py tests/test_SwitchingContract.py tests/test_sub_account.py
 python3 utils/send_email.py
