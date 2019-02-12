a8bd5d42c576ff8ee947e9c4982209663d5f373e
Xiawang <xiawang@lagou.com>
2019-02-12 16:55:49 +0800

    调整run_jianzhao_web.sh配置文件

--- run_jianzhao_web.sh
+++ run_jianzhao_web.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #/usr/local/python3/bin/pipenv run pytest tests/test_b_basic_process.py tests/test_B_add_people_into_company.py tests/test_candidate.py tests/test_interview.py tests/test_job_inviation.py tests/test_open_channel.py tests/test_postposition.py tests/test_resume_clean.py tests/test_search_optimization_phase_II.py tests/test_talent.py
-/usr/local/python3/bin/pipenv run pytest tests/test_zhaopin_web/
+/usr/local/python3/bin/pipenv run pytest tests/test_jianzhao_web/
