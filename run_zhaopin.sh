5e09a42c78b91f33a7a54c4815186af0a4562865
Xiawang <xiawang@lagou.com>
2019-02-14 15:25:09 +0800

    修改脚本配置

--- run_zhaopin.sh
+++ run_zhaopin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/python3/bin/pipenv run pytest tests/test_app_b_chat.py tests/test_app_b_position.py
+/usr/local/python3/bin/pipenv run pytest tests/test_zhaopin_app/test_app_b_chat.py tests/test_zhaopin_app/test_app_b_position.py
