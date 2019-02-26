c839e238be74bc919048e4358753296cc580de0c
yqzhang <yqzhang@lagou.com>
2019-02-26 16:18:15 +0800

    拉勾加试用版接口和pytest

--- api_script/zhaopin_app/b_goods.py
+++ api_script/zhaopin_app/b_goods.py
@@ -11,3 +11,24 @@ def goods_product_version():
 	url = host + "/goods/product_version"
 	remark = "获取当前用户商业产品版本号"
 	return json_post(url=url, headers=headers, remark=remark)
+
+
+# yq新增:2019.2.16
+def getPopUpData():
+	url = host+'/probation/getPopUpData'
+	remark = '试用期弹窗'
+	return json_post(url=url, headers=headers, remark=remark)
+
+def getRightsList ():
+	url = host+'/rights/getRightsList'
+	remark = '查询我的权益'
+	return json_post(url=url, headers=headers, remark=remark)
+
+def getUserInfo ():
+	url = host+'/userInfo/getUserInfo'
+	remark = '获取用户信息'
+	return json_post(url=url, headers=headers, remark=remark)
+
+# getPopUpData()
+# getRightsList()
+# getUserInfo()
