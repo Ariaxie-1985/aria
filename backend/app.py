4495d89965ff371774447bfeac420255233da484
Xiawang <xiawang@lagou.com>
2019-03-05 14:51:42 +0800

    新增获取pytest测试报告的接口，调整执行pytest的接口逻辑

--- backend/app.py
+++ backend/app.py
@@ -37,6 +37,6 @@ api.add_resource(B_Basic_Process, '/jianzhao/company/registration')
 api.add_resource(B_Add_People_Into_Company, '/jianzhao/personal/registration')
 
 api.add_resource(C_Basic_Process, '/customer/registration')
-api.add_resource(run_Pytest, '/outward/pytest')
+api.add_resource(run_Pytest, '/pytest')
 
 app.run(debug=True, host='0.0.0.0', port=9004)
