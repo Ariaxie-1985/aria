655951907417f6bffd6271441e43b5a1755da7d7
yqzhang <yqzhang@lagou.com>
2019-03-06 14:52:21 +0800

    calling修改，setup和teardown

--- tests/test_business/test_B_calling.py
+++ tests/test_business/test_B_calling.py
@@ -2,28 +2,23 @@
 import pytest
 
 from api_script.business.B_calling import calling
-from utils.util import login
+from utils.util import login ,assert_equal
 import logging
 from utils.read_file import get_yaml_test_data
 
-cUserid = 100012422
+# cUserid = 100012422
+def setup_module(module):
+    pass
+def teardown_module(module):
+    pass
 
-
-@pytest.mark.skip(reason="暂时注掉先不跑")
+# @pytest.mark.skip(reason="暂时注掉先不跑")
 def test_calling(login_web_k8s_env_b):
-    # 暂时注掉先不跑
-
-    s = calling(cUserid)
+    s = calling(80)
     logging.getLogger().setLevel(logging.INFO)
+    # assert s['message'] == '成功'
+    assert_equal( '成功',s['message'],'calling获取成功,虚拟号码：' + str(s['content']['data']['result']['virtualPhone']),'calling获取失败，响应信息：' + str(s))
 
-    # if s['message'] == u'成功':
-    try:
-        assert s['message'] == '成功'
-        logging.info(u'calling获取成功,虚拟号码：' + str(s['content']['data']['result']['virtualPhone']))
 
-    # print(s['content']['data']['result']['virtualPhone'])
-    except:
-        logging.info(u'calling获取失败，响应信息：' + str(s))
-    # print(s)
 
 # test_calling()
