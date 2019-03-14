1852edbdc719d76640b83299817b1e20a90fbfe7
Xiawang <xiawang@lagou.com>
2019-03-14 15:41:38 +0800

    提交C端投递简历给职位

--- api_script/jianzhao_web/b_position/B_offlineposition.py
+++ api_script/jianzhao_web/b_position/B_offlineposition.py
@@ -1,25 +1,40 @@
 # coding:utf-8
 # @Author: Xiawang
-from utils.util import form_post,get_code_token, login
+from utils.util import form_post, get_code_token, login
 
 '''
 职位 - 下线 在线职位列表的第一个职位
 '''
 
-username = 20181205
-login("00852",username)
 
 # 下线职位
-refer_offlinePosition_url = "https://easy.lagou.com/position/multiChannel/myOnlinePositions.htm?pageNo=1"
-Position_header = get_code_token(refer_offlinePosition_url)
-myonlinepostions_url = "https://easy.lagou.com/parentPosition/multiChannel/myOnlinePositions.json"
-myonlinepostions_data = {"pageNo":1}
-remark = "获取下线职位的职位id"
-r = form_post(url=myonlinepostions_url, data=myonlinepostions_data,headers=Position_header,remark=remark)
-positionId = r['content']['data']['parentPositionVOs'][0]['positions'][0]['positionId']
+def get_online_positionId():
+    refer_offlinePosition_url = "https://easy.lagou.com/position/multiChannel/myOnlinePositions.htm?pageNo=1"
+    Position_header = get_code_token(refer_offlinePosition_url)
+    myonlinepostions_url = "https://easy.lagou.com/parentPosition/multiChannel/myOnlinePositions.json"
+    myonlinepostions_data = {"pageNo": 1}
+    remark = "获取下线职位的职位id"
+    r = form_post(url=myonlinepostions_url, data=myonlinepostions_data, headers=Position_header, remark=remark)
+    positionId = r['content']['data']['parentPositionVOs'][0]['positions'][0]['positionId']
+    return positionId
 
-Position_header = get_code_token(refer_offlinePosition_url)
-offlinePosition_url = "https://easy.lagou.com/position/offlinePosition.json"
-offlinePosition_data = {"positionId":positionId}
-remark = "下线职位"
-form_post(url=offlinePosition_url, data=offlinePosition_data,headers=Position_header,remark=remark)
+
+def offlinePosition(positionId):
+    refer_offlinePosition_url = 'https://easy.lagou.com/position/multiChannel/myOnlinePositions.htm?pageNo=1'
+    Position_header = get_code_token(refer_offlinePosition_url)
+    offlinePosition_url = "https://easy.lagou.com/position/offlinePosition.json"
+    offlinePosition_data = {"positionId": positionId}
+    remark = "下线职位"
+    return form_post(url=offlinePosition_url, data=offlinePosition_data, headers=Position_header, remark=remark)
+
+
+def online_positionId_outerPositionId():
+    refer_offlinePosition_url = "https://easy.lagou.com/position/multiChannel/myOnlinePositions.htm?pageNo=1"
+    Position_header = get_code_token(refer_offlinePosition_url)
+    myonlinepostions_url = "https://easy.lagou.com/parentPosition/multiChannel/myOnlinePositions.json"
+    myonlinepostions_data = {"pageNo": 1}
+    remark = "获取下线职位的职位id"
+    r = form_post(url=myonlinepostions_url, data=myonlinepostions_data, headers=Position_header, remark=remark)
+    positionId = r['content']['data']['parentPositionVOs'][0]['positions'][0]['positionId']
+    outerPositionId = r['content']['data']['parentPositionVOs'][0]['positions'][0]['outerPositionId']
+    return positionId, outerPositionId
