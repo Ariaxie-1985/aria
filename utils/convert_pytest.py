321f4968bd3c8d506f62956d5db529ea76241d83
Xiawang <xiawang@lagou.com>
2019-03-26 19:34:22 +0800

    postman脚本转pytest用例代码初步提交

---
+++ utils/convert_pytest.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+# @Time  : 2019-03-26 17:15
+# @Author: Xiawang
+import json
+
+file_path = '/Users/wang/Downloads/言职社区通知页优化需求.postman_collection.json'
+
+
+def read_json(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+            return data
+    except:
+        return "文件找不到, 请确认路径是否正确"
+
+
+def parser(data):
+    while len(data['item']):
+        for t in data['item']:
+            if 'item' in t:
+                pass
+            else:
+
+
+
+        yield
+
+
+r = read_json()
+print(r)
