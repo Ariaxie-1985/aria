b19ceadbefe0cc6c14705fafd845958b50a36ba8
yqzhang <yqzhang@lagou.com>
2019-03-28 11:04:05 +0800

    修改接口url

--- api_script/entry/order/hasDelivered.py
+++ api_script/entry/order/hasDelivered.py
@@ -3,12 +3,12 @@ __author__ = 'yqzhang'
 from utils.util import get_app_header, get_requests, json_post
 import random
 header=get_app_header(100018375)
-alist = random.sample(range(1,10000000),50)
+# alist = random.sample(range(1,10000000),50)
 def hasDelivered():
-    url='https://gate.lagou.com/v1/entry/order/hasDelivered'
+    url='https://gate.lagou.com/v1/entry/order/deliverStatus'
     data={'positionIds':5378018}
     return get_requests(url=url,headers=header,data=data,remark='职位是否已投递')
 
 
-# print (alist)
+# print (5378018)
 # hasDelivered()
