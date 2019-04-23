0343eee073595ef63fd6ffc579510501ffd09375
betty <betty@lagou.com>
2019-04-23 19:40:36 +0800

    增加福利验证

--- tests/test_neirong_app/test_create_fuli.py
+++ tests/test_neirong_app/test_create_fuli.py
@@ -3,8 +3,9 @@
 
 import pytest
 from api_script.neirong.company.create_fuli import create_fuli
+from api_script.neirong.company.view_fuli import view_fuli
 from utils.util import assert_equal
-from tests.test_neirong_app.test_view_fuli import test_view_fuli_info
+from tests.test_neirong_app.test_view_fuli import test_view_fuli
 
 
 def setup_module():
@@ -16,10 +17,10 @@ def teardown_module():
 def test_create_fuli():
     r = create_fuli()
     print(r)
-    assert_equal(1,r['state'],"增加公司福利")
+    assert_equal(1, r['state'], "增加公司福利")
 
 
-# def test_view_info():
-#     r = test_view_fuli_info()
-#     print(r)
-#     assert_equal(1,r['state'],"获取公司福利")
+def test_view_fuli():
+    r = view_fuli()
+    print(r)
+    assert_equal(1, r['state'], "获取公司福利")
