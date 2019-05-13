4ce0a845a08621e2fbf0e76167f867c54b8d405f
yqzhang <yqzhang@lagou.com>
2019-05-13 18:26:17 +0800

    解决冲突

--- api_script/entry/buser/hrinfo.py
+++ api_script/entry/buser/hrinfo.py
@@ -8,22 +8,10 @@ header = {"Accept": "application/json", "X-L-REQ-HEADER": {"deviceType": 10}, "X
           "X-L-DA-HEADER": "da5439aadaf04ade94a214d730b990d83ec71d3e9f274002951143c843badffbc543b213dfe84e21a37bb782dd9bbca4be8d947ead7041f79d336cb1217127d15"}
 header["X-L-REQ-HEADER"] = json.dumps(header["X-L-REQ-HEADER"])
 
-<<<<<<< HEAD
-header = get_app_header(100014641)
-||||||| merged common ancestors
-header=get_app_header(100014641)
-=======
->>>>>>> yq
 
 def hrinfo():
     url = 'https://gate.lagou.com/v1/entry/buser/hrInfo/100014641'
     return get_requests(url=url, headers=header, remark='hr信息')
 
-<<<<<<< HEAD
-#hrinfo()
-||||||| merged common ancestors
-# hrinfo()
-=======
 
 # print(hrinfo())
->>>>>>> yq
