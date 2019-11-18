c6f05fe67aa7c16cd5e4ae7fb265c1ff65873e67
Xiawang <xiawang@lagou.com>
2019-11-18 17:45:22 +0800

    注释和数据库相关的

--- backend/resources/spring/urls.py
+++ backend/resources/spring/urls.py
@@ -1,15 +1,15 @@
-# coding:utf-8
-# @Time  : 2019-08-03 23:41
-# @Author: Xiawang
-from flask_restful import Api
-
-from backend.resources.spring import spring
-from backend.resources.spring.my_testsheets import MyTestSheets
-from backend.resources.spring.testsheet import SprintTestSheet
-
-api = Api(spring)
-
-api.add_resource(SprintTestSheet, '/testsheet')
-api.add_resource(MyTestSheets, '/mytestsheet')
-
-import backend.resources.spring.urls
+# # coding:utf-8
+# # @Time  : 2019-08-03 23:41
+# # @Author: Xiawang
+# from flask_restful import Api
+#
+# from backend.resources.spring import spring
+# from backend.resources.spring.my_testsheets import MyTestSheets
+# from backend.resources.spring.testsheet import SprintTestSheet
+#
+# api = Api(spring)
+#
+# api.add_resource(SprintTestSheet, '/testsheet')
+# api.add_resource(MyTestSheets, '/mytestsheet')
+#
+# import backend.resources.spring.urls
