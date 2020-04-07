8f446ecabee45f68c6ed9f8f515ee6ed985549da
Xiawang <xiawang@lagou.com>
2020-04-07 17:50:57 +0800

    header 调整

--- api_script/neirong_app/company_tab.py
+++ api_script/neirong_app/company_tab.py
@@ -5,8 +5,8 @@
 from utils.util import app_header_999, get_requests
 
 
-def queryCompanyList(city, userToken, ip_port=None):
+def queryCompanyList(city, userToken, userId=None, ip_port=None):
     url = "https://gate.lagou.com/v1/neirong/companyTab/queryCompanyList?pageNo=0&pageSize=20&city={}".format(city)
-    header = app_header_999(userToken, DA=False)
+    header = app_header_999(userToken, DA=False, userId=userId)
     remark = '查询公司TAB页'
     return get_requests(url=url, headers=header, ip_port=ip_port, remark=remark).json()
