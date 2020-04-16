90fe172ed595fd9de3ba089c905e43d5c9749b52
Xiawang <xiawang@lagou.com>
2020-04-16 10:26:37 +0800

    调试B端转发简历

--- api_script/weapp/api.py
+++ api_script/weapp/api.py
@@ -2,6 +2,8 @@
 # @Time  : 2020/4/14 16:38
 # @Author: Xiawang
 # Description:
+from api_script.entry.account.passport import password_login
+from api_script.zhaopin_app.talent import talent_recTalent, talent_info_get
 from utils.util import app_header_999, get_requests
 
 
@@ -27,3 +29,16 @@ def session_share_info_app_b_yun(userToken, orderResumeId, ip_port=None, userId=
     url = f'https://gate.lagou.com/v1/weapp/api-yun/resume/{orderResumeId}/session-share-info-app-b'
     header = app_header_999(userToken=userToken, userId=userId)
     return get_requests(url=url, headers=header, remark='B端用户与C端用户的消息里分享简历到微信好友', ip_port=ip_port).json()
+
+
+if __name__ == '__main__':
+    r = password_login("19910626899", "000000")
+    userToken = r['content']['userToken']
+    r = talent_recTalent(userToken=userToken, positionId='7027535')
+    resumeId = r['content']['result'][0]['resumeId']
+    r =session_share_info_app_b_corp(userToken=userToken,resumeId=resumeId)
+    print(r)
+    r = talent_info_get(userToken=userToken, userId=15166231)
+    long_resumeId = r['content']['resumeId']
+    r = session_share_info_app_b_yun(userToken=userToken, orderResumeId=long_resumeId)
+    print(r)
