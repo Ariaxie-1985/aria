02f4f3790681e71f4d1b495f4d33b59b53ff246b
Xiawang <xiawang@lagou.com>
2020-05-25 17:38:29 +0800

    注释主流程脚本的conftest脚本

--- tests/test_mainprocess/conftest.py
+++ tests/test_mainprocess/conftest.py
@@ -14,6 +14,22 @@ from api_script.jianzhao_web.b_basic.toB_saveHR_1 import get_b_person_userId, ge
 
 fake = Faker("zh_CN")
 
+'''
+1.数据共享
+
+@pytest.fixture()
+使用场景:
+1.1.为测试用例传参, 起到数据共享作用
+1.2.可以做测试用例的初始化操作
+
+作用域
+1.1.默认function: 一个函数内部共享一个fixture
+1.2.class: 一个class内共享同一个fixture
+1.3.module: 一个.py文件内共享同一个fixture
+1.4.session:当前目录下多个.py文件共享同一个fixture
+    
+'''
+
 
 @pytest.fixture(scope='session')
 def get_company_name():
@@ -34,11 +50,13 @@ def get_countryCode_phone_general_user():
     user_name = '拉勾测试自动化' + fake.name()
     return countryCode, phone, user_name
 
+
 @pytest.fixture(scope='session')
 def get_countryCode_phone_general_user_01():
     countryCode, phone = "00852", str(20000000 + int(str(time.time()).split('.')[1]))
     user_name = '拉勾测试自动化' + fake.name()
     return countryCode, phone, user_name
+
 
 @pytest.fixture()
 def get_user_id():
@@ -111,7 +129,15 @@ def c_login_education(request):
     return result['content']['userToken'], result['content']['userInfo']['userId']
 
 
+# 2.当某用例失败后,接下来的依赖用例直接标记失败,不执行
+# 用 pytest_configure(), pytest_runtest_setup(), pytest_runtest_makereport()三个函数共同合作的
 def pytest_runtest_makereport(item, call):
+    '''
+    在第一个没有报失败的用例停止执行
+    :param item: 测试用例
+    :param call:调用步骤,pytest_runtest_setup(item), pytest_runtest_call(item), pytest_runtest_teardown(item)
+    :return:一个测试报告对象
+    '''
     if "incremental" in item.keywords:
         if call.excinfo is not None:
             parent = item.parent
@@ -119,6 +145,12 @@ def pytest_runtest_makereport(item, call):
 
 
 def pytest_runtest_setup(item):
+    '''
+    called before ``pytest_runtest_call(item)
+    每个测试用例的初始化,如果上一个用例是failed,接下来的用例有失败的, 就标记 xfail
+    :param item: 测试用例
+    :return:
+    '''
     if "incremental" in item.keywords:
         previousfailed = getattr(item.parent, "_previousfailed", None)
         if previousfailed is not None:
@@ -126,6 +158,11 @@ def pytest_runtest_setup(item):
 
 
 def pytest_configure(config):
+    '''
+    编写新的hook函数
+    :param config: pytest命令配置
+    :return: 增加name:markers, 命令:incremental，备注:mark test to run only on named main_process的配置
+    '''
     config.addinivalue_line(
         "markers", "incremental: mark test to run only on named main_process"
     )
