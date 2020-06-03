e87bf7602b32e6bb03fc0ab166ae014588d6b29a
Xiawang <xiawang@lagou.com>
2020-06-03 15:11:08 +0800

    提交职位的测试脚本

--- api_script/open_lagou_com/position.py
+++ api_script/open_lagou_com/position.py
@@ -9,21 +9,21 @@ from utils.util import get_requests, form_post
 
 
 def category_get(access_token):
-    url = 'https://open.lagou.com/position/category/get'
+    url = 'https://open.lagou.com/v1/position/category/get'
     data = {'access_token': access_token}
     remark = '获取职位类别'
     return get_requests(url=url, data=data, remark=remark)
 
 
 def company_address_list(access_token, page_no=1, page_size=20):
-    url = 'https://open.lagou.com/position/address/company/list'
+    url = 'https://open.lagou.com/v1/position/address/company/list'
     data = {'access_token': access_token, 'page_no': page_no, 'page_size': page_size}
     remark = '获取公司的所有地址'
     return get_requests(url=url, data=data, remark=remark)
 
 
 def company_address_district(access_token):
-    url = 'https://open.lagou.com/position/address/district/get'
+    url = 'https://open.lagou.com/v1/position/address/district/get'
     data = {'access_token': access_token}
     remark = '获取省市区'
     return get_requests(url=url, data=data, remark=remark)
@@ -35,7 +35,7 @@ def company_address_create(access_token, openid, **kwargs):
     area = kwargs.get('area', '朝阳区')
     detail = kwargs.get('detail', '爱奇艺大厦')
 
-    url = f'https://open.lagou.com/position/address/create?access_token={access_token}&openid={openid}'
+    url = f'https://open.lagou.com/v1/position/address/create?access_token={access_token}&openid={openid}'
     data = {'province': province, 'city': city, 'area': area, 'detail': detail}
     remark = '获取省市区'
     return form_post(url=url, data=data, remark=remark)
@@ -84,10 +84,19 @@ def get_position_info(access_token, position_id):
 
 
 def update_position_info(access_token, **kwargs):
+    position_id = kwargs.get('position_id', 0)
+    address_id = kwargs.get('address_id', 0)
     max_salary = kwargs.get('max_salary', 25)
     min_salary = kwargs.get('min_salary', 20)
+    position_nature = kwargs.get('position_nature', '全职')
+    work_year = kwargs.get('work_year', '3-5年')
+    education = kwargs.get('education', '本科')
+    advantage = kwargs.get('advantage', '20薪')
+    detail = kwargs.get('detail', '肯加班拿钱少肯加班拿钱少肯加班拿钱少肯加班拿钱少肯加班拿钱少肯加班拿钱少肯加班拿钱少')
     url = f'https://open.lagou.com/v1/position/update?access_token={access_token}'
-    data = {'max_salary': max_salary, 'min_salary': min_salary}
+    data = {'position_id': position_id, 'work_year': work_year, 'position_nature': position_nature,
+            'max_salary': max_salary, 'education': education, 'advantage': advantage,
+            'min_salary': min_salary, 'detail': detail, 'address_id': address_id}
     remark = '更新职位信息'
     return form_post(url=url, data=data, remark=remark)
 
@@ -113,13 +122,13 @@ def refresh_position(access_token, position_id):
     return form_post(url=url, data=data, remark=remark)
 
 
-def get_position_list(access_token):
+def get_position_list(access_token, status='ONLINE'):
     url = f'https://open.lagou.com/v1/position/list'
     today = datetime.datetime.now()
     start_time = int(str(time.mktime(time.strptime((today - datetime.timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S"),
                                                    '%Y-%m-%d %H:%M:%S')) * 1000).split(".")[0])
-    end_time = time.time()
-    data = {'access_token': access_token, 'start_time': start_time, 'end_time': end_time}
+    end_time = int(str(int(time.time())) + '000')
+    data = {'access_token': access_token, 'start_time': start_time, 'end_time': end_time, 'status': status}
     remark = '获取职位列表信息'
     return get_requests(url=url, data=data, remark=remark)
 
--- tests/test_open_api_lagou_com/conftest.py
+++ tests/test_open_api_lagou_com/conftest.py
@@ -4,12 +4,22 @@
 # Description:
 import pytest
 
+from api_script.open_lagou_com.account import openid_query
 from api_script.open_lagou_com.authority import open_authority_token
+
+access_token_list = []
 
 
 @pytest.fixture(scope='session')
 def get_access_token():
     res = open_authority_token(appid='b1a5rsdsi7qukd3x', secret='2lvvus0lztf692u4vt9hqxare5qbm9ap',
                                grant_type='client_credential')
+    access_token = res.get('access_token')
+    access_token_list.append(access_token)
+    return access_token
 
-    return res.get('access_token')
+
+@pytest.fixture(scope='session')
+def get_openid():
+    res = openid_query(access_token=access_token_list[0], credential=13033647506)
+    return res.get('openid')
