2bf9c7c308b4190374e076716b048372119c6ac8
Sunnyzhang <sunnyzhang@lagou.com>
2020-06-10 19:54:09 +0800

    代码冲突

--- api_script/education/account.py
+++ api_script/education/account.py
@@ -3,14 +3,14 @@
 # @Author: Sunnyzhang
 # Description:
 from api_script.entry.account.passport import password_login
-from utils.util import  get_requests,app_header_999
+from utils.util import  get_requests,get_edu_app_header
 
 
 
 def getToken(userToken):
     url = 'https://gate.lagou.com/v1/entry/account/h5/getToken'
     # header = get_header(url="https://kaiwu.lagou.com/distribution/appCenter.html")
-    header = app_header_999(userToken=userToken,DA=False,appType=1)
+    header = get_edu_app_header(userToken=userToken,DA=False,appType=1)
     header["appVersion"]="1.2.7.680"
     header["reqVersion"] = "10207"
     header["lgId"] = "862502040661300_1591588692323"
