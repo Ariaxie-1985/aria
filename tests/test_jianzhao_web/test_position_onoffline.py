fdcf817a8d67130fbb1b2e67f677561e5daca756
Bingoonchen <bingoonchen@lagou.com>
2020-06-11 19:09:36 +0800

    职位下线及再发布用例脚本

--- tests/test_jianzhao_web/test_position_onoffline.py
+++ tests/test_jianzhao_web/test_position_onoffline.py
@@ -9,7 +9,6 @@ from utils.util import assert_equal,assert_not_equal
 def test_offline_position(my_login_password):
     #获取在线职位id
     positionid = get_online_positionId()
-    print(positionid)
     #下线职位
     status = offline_position(positionid)
     assert_equal (1, status['state'], "职位下线成功",f"职位下线失败:{status.get('message')}" )
