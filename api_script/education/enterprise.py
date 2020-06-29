176eb5764b2bf3e4e4a1903877be216e62826be0
ariaxie1985 <56579847+ariaxie1985@users.noreply.github.com>
2020-06-29 11:17:58 +0800

    更新企业_id

--- api_script/education/enterprise.py
+++ api_script/education/enterprise.py
@@ -4,6 +4,6 @@ from utils.util import login_password, get_requests
 def search_staff():
     search_referer_url = 'https://kaiwu.lagou.com/enterprise/index.html'
     get_requests(search_referer_url)
-    search_url = 'https://gate.lagou.com/v1/neirong/edu/enterprise/getStaffsByStudyState?studyState=STUDY&phone=&name=bj&pageNo=1&pageSize=20&enterpriseId=441'
+    search_url = 'https://gate.lagou.com/v1/neirong/edu/enterprise/getStaffsByStudyState?studyState=STUDY&phone=&name=bj&pageNo=1&pageSize=20&enterpriseId=2'
     response = get_requests(url = search_url, headers={'x-l-req-header':'{deviceType:1}'})
     return response
