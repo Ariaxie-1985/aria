4ac3829b56075c4c9cda37581e0be100cc17ca57
arayang <arayang@lagou.com>
2020-06-30 16:32:40 +0800

    修改关键字参数名

--- tests/test_mainprocess/test_education.py
+++ tests/test_mainprocess/test_education.py
@@ -147,6 +147,6 @@ def test_exchange_present(c_login_education_verifycode,get_edu_h5_token):
     sendverigycode=send_verify_code(countrycode,phone,'PASSPORT_REGISTER')
     verify_code = verify_code_message(countryCode=countrycode, phone=phone)
     #verify_code="049281"
-    verifyCode_login(countrycode=countrycode,phone=phone,verify_code=verify_code,app_type='LGEdu')
+    verifyCode_login(countryCode=countrycode,phone=phone,verify_code=verify_code,app_type='LGEdu')
     register_by_phone(countryCode=countrycode, phone=phone, verify_code=verify_code)
 
