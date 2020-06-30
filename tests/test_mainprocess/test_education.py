ed45bfaf9ed23b13a97e4e03c5672bd1c00c328d
arayang <arayang@lagou.com>
2020-06-30 20:30:49 +0800

    modify bug

--- tests/test_mainprocess/test_education.py
+++ tests/test_mainprocess/test_education.py
@@ -146,7 +146,7 @@ def test_exchange_present(c_login_education_verifycode,get_edu_h5_token):
     phone=c_login_education_verifycode[2]
     print(phone)
     #phone="2020062700"
-    sendverigycode=send_verify_code(countrycode,phone,'PASSPORT_REGISTER',app_type='LGEdu')
+    sendverigycode=send_verify_code(countrycode=countrycode,phone=phone,businessType='PASSPORT_REGISTER',app_type='LGEdu')
     time.sleep(12)
     verify_code = verify_code_message(countryCode=countrycode, phone=phone)
     print(verify_code)
