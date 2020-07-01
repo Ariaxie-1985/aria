03f92302cb61356aec082c0ba0ae9d86c271d17d
arayang <arayang@lagou.com>
2020-07-01 17:18:28 +0800

    加打印

--- tests/test_mainprocess/test_education.py
+++ tests/test_mainprocess/test_education.py
@@ -143,7 +143,7 @@ class TestUserGrowth(object):
         r = receive_credit(gateLoginToken=get_edu_h5_token)
         receive_success = r['content']
         assert_equal(True, bool(receive_success), "登录学分领取成功", te='杨彦')
-    def test_usableCredit(self,get_edu_h5_token):
+    def test_usable_credit(self,c_login_education_0044,get_edu_h5_token):
         global courseCredit
         r = get_credit_center_info(userToken=get_edu_h5_token)
         print("######")
