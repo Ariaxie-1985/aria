7f588244d2894f22831c5b9d2357dc5a2ae21ee7
Sunnyzhang <sunnyzhang@lagou.com>
2020-07-01 14:10:43 +0800

    添加接口负责人

--- api_script/education/edu.py
+++ api_script/education/edu.py
@@ -10,7 +10,7 @@ def get_course_list(userToken):
     url = "https://gate.lagou.com/v1/neirong/edu/homepage/getCourseList"
     header = get_edu_app_header(userToken=userToken, DA=False)
     remark = "获取专栏课程信息"
-    result = get_requests(url=url, headers=header, remark=remark)
+    result = get_requests(url=url, headers=header, remark=remark,rd='Yuwei Cheng')
     a = result['content']['courseCardList'][0]['courseList']
     for k in a:
         if k['hasBuy']:
