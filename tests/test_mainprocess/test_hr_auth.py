70c0b1917f05bf553451c3fea4fcb865eeef69d2
Xiawang <xiawang@lagou.com>
2020-07-06 16:26:38 +0800

    删除多余的import

--- tests/test_mainprocess/test_hr_auth.py
+++ tests/test_mainprocess/test_hr_auth.py
@@ -2,21 +2,16 @@
 # @Time  : 2020-06-11 16:45
 # @Author: Xiawang
 # Description:认证前hr可发职位，使用在职证明+手持身份证方式可以成功提交认证,通过后有一个提审前发布的职位是在线状态
-#管理员协助通过招聘审核流程
-
+# 管理员协助通过招聘审核流程
 import time
-
 import pytest
-import json
-
 from api_script.jianzhao_web.b_basic.admin_review import admin_review
 from api_script.jianzhao_web.b_basic.b_upload import upload_incumbency_certification
-from api_script.jianzhao_web.b_basic.toB_saveHR_1 import saveHR, add_saveCompany, submit_new, remove_member, \
-    close_trial_package, remove_member_has_offline_position, recruiter_members
+from api_script.jianzhao_web.b_basic.toB_saveHR_1 import saveHR, add_saveCompany, submit_new, \
+    remove_member_has_offline_position, recruiter_members
 from api_script.jianzhao_web.b_position.B_postposition import createPosition_999, get_online_positions, offline_position
 from api_script.jianzhao_web.talent.unauth_positon_talent_rec import talent_rec_unAuth
 from api_script.neirong_app.account import upate_user_password
-from tests.test_B_enter.conftest import get_positionType, get_user_info
 
 from utils.loggers import logers
 from utils.util import assert_equal, assert_in, pc_send_register_verifyCode, verify_code_message, user_register_lagou, \
@@ -45,12 +40,14 @@ class TestHRAuth(object):
         global register_state
         register = user_register_lagou(hr1_countryCode, hr1_phone, verify_code)
         register_state = register.get('state', 0)
-        assert_equal(expect_value=1, actual_value=register_state, success_message='校验hr1注册成功', fail_message='失败手机号:{}'.format(hr1_countryCode + hr1_phone), te='唐欣洁')
+        assert_equal(expect_value=1, actual_value=register_state, success_message='校验hr1注册成功',
+                     fail_message='失败手机号:{}'.format(hr1_countryCode + hr1_phone), te='唐欣洁')
 
-    @pytest.mark.parametrize('companyFullName,resumeReceiveEmail,userPosition',[('拉勾测试自动化公司00911111111111','foxtang01@lagou.com','测试工程师')])
-    def test_save_hr1_info(self,companyFullName,resumeReceiveEmail,userPosition):
-        r= saveHR(companyFullName, hr1_user_name, resumeReceiveEmail,userPosition)
-        assert_equal(1, r.get('state',0), '验证hr基本信息保存成功',te='唐欣洁')
+    @pytest.mark.parametrize('companyFullName,resumeReceiveEmail,userPosition',
+                             [('拉勾测试自动化公司00911111111111', 'foxtang01@lagou.com', '测试工程师')])
+    def test_save_hr1_info(self, companyFullName, resumeReceiveEmail, userPosition):
+        r = saveHR(companyFullName, hr1_user_name, resumeReceiveEmail, userPosition)
+        assert_equal(1, r.get('state', 0), '验证hr基本信息保存成功', te='唐欣洁')
 
     def test_hr1_join_company(self):
         join_company = add_saveCompany()
@@ -66,8 +63,8 @@ class TestHRAuth(object):
 
     def test_unAuth_position_rec(self):
         r = talent_rec_unAuth(unAuth_positionId)
-        #这里不判断人才数量了，由于业务逻辑限制，不适合频繁推真正的人才出来
-        assert_equal(1,r.get('state',0),'验证未发布职位推荐人才简历接口访问成功', te='唐欣洁')
+        # 这里不判断人才数量了，由于业务逻辑限制，不适合频繁推真正的人才出来
+        assert_equal(1, r.get('state', 0), '验证未发布职位推荐人才简历接口访问成功', te='唐欣洁')
 
     def test_hr1_upload_incumbency_certification(self):
         r = upload_incumbency_certification()
@@ -93,10 +90,10 @@ class TestHRAuth(object):
     @pytest.mark.parametrize('newPassword', [('990eb670f81e82f546cfaaae1587279a')])
     def test_update_hr1_password(self, newPassword):
         r = upate_user_password(newPassword)
-        assert_equal(1, r['state'], 'hr1修改密码成功',te='唐欣洁')
+        assert_equal(1, r['state'], 'hr1修改密码成功', te='唐欣洁')
         login_password(hr1_countryCode + hr1_phone, newPassword)
 
-    def test_recruiter_members_hr1(self,get_user_info):
+    def test_recruiter_members_hr1(self, get_user_info):
         global hr1Id, easy_company_id, www_company_id
         hr1Id, easy_company_id, www_company_id = get_user_info
         r = recruiter_members()
@@ -110,18 +107,20 @@ class TestHRAuth(object):
         remove_result = remove_member_has_offline_position()
         assert_equal(1, remove_result.get('state'), '校验移除hr1的招聘者服务成功！', te='唐欣洁')
 
-    @pytest.mark.parametrize('companyFullName,resumeReceiveEmail,userPosition',[('拉勾测试自动化公司00911111111112','foxtang01@lagou.com','测试工程师')])
-    def test_save_hr1_info02(self,companyFullName,resumeReceiveEmail,userPosition):
-        personal_info_save = saveHR(companyFullName, hr1_user_name, resumeReceiveEmail,userPosition)
+    @pytest.mark.parametrize('companyFullName,resumeReceiveEmail,userPosition',
+                             [('拉勾测试自动化公司00911111111112', 'foxtang01@lagou.com', '测试工程师')])
+    def test_save_hr1_info02(self, companyFullName, resumeReceiveEmail, userPosition):
+        personal_info_save = saveHR(companyFullName, hr1_user_name, resumeReceiveEmail, userPosition)
         assert_equal(1, personal_info_save.get('state', 0), '验证hr基本信息保存成功', te='唐欣洁')
 
     def test_hr1_join_company02(self):
         join_company = add_saveCompany()
         assert_equal(1, join_company.get('state', 0), '验证加入公司是否成功', te='唐欣洁')
 
-    @pytest.mark.parametrize('companyFullName,resumeReceiveEmail,userPosition',[('拉勾测试自动化公司00911111111111','foxtang01@lagou.com','测试工程师')])
-    def test_save_hr1_info03(self,companyFullName,resumeReceiveEmail,userPosition):
-        personal_info_save = saveHR(companyFullName, hr1_user_name, resumeReceiveEmail,userPosition)
+    @pytest.mark.parametrize('companyFullName,resumeReceiveEmail,userPosition',
+                             [('拉勾测试自动化公司00911111111111', 'foxtang01@lagou.com', '测试工程师')])
+    def test_save_hr1_info03(self, companyFullName, resumeReceiveEmail, userPosition):
+        personal_info_save = saveHR(companyFullName, hr1_user_name, resumeReceiveEmail, userPosition)
         assert_equal(1, personal_info_save.get('state', 0), '验证hr1基本信息保存成功', te='唐欣洁')
 
     def test_hr1_join_company03(self):
@@ -129,16 +128,16 @@ class TestHRAuth(object):
         assert_equal(1, join_company.get('state', 0), '验证更换公司加入成功', te='唐欣洁')
 
     def test_manager_assist(self):
-        login_password('00911111111111','c47eeb69fa4e64971fb29cb1e9163a19')
+        login_password('00911111111111', 'c47eeb69fa4e64971fb29cb1e9163a19')
         r = admin_review(hr1Id)
-        assert_equal(1,r.get('state',0),'验证管理员协助通过审核成功', te='唐欣洁')
+        assert_equal(1, r.get('state', 0), '验证管理员协助通过审核成功', te='唐欣洁')
 
-    def test_hr1_company(self,get_password,get_user_info):
+    def test_hr1_company(self, get_password, get_user_info):
         login_password(hr1_countryCode + hr1_phone, get_password)
         hr1Id, easy_company_id, www_company_id = get_user_info
-        assert_equal('783664',easy_company_id,'验证当前公司是更换公司之后的所属公司', te='唐欣洁')
+        assert_equal('783664', easy_company_id, '验证当前公司是更换公司之后的所属公司', te='唐欣洁')
 
-    def test_hr1_publish_free_position(self,get_positionType):
+    def test_hr1_publish_free_position(self, get_positionType):
         r = createPosition_999(firstType=get_positionType[0], positionType=get_positionType[1],
                                positionThirdType=get_positionType[2],
                                positionName=get_positionType[3])
