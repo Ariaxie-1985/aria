b367e444bf3913c2d0b7388ce6e3f095de41c731
Sunnyzhang <sunnyzhang@lagou.com>
2020-07-13 11:47:02 +0800

    xiug

--- tests/test_mainprocess/11.py
+++ tests/test_mainprocess/11.py
@@ -1,11 +1,11 @@
-# a =  None
-# if a >3:
-#     print(a)
-# else:
-#     print("a不大于3")
-orderNo = {}
-a = 3
-id = [1,2,3,4,5]
-for i in id:
-    orderNo.update({i:a})
-print(orderNo)
+a =  -1
+if a >3:
+    print(a)
+else:
+    print("a不大于3")
+# orderNo = {}
+# a = 3
+# id = [1,2,3,4,5]
+# for i in id:
+#     orderNo.update({i:a})
+# print(orderNo)
